3a346ee3170d4adda63aec96c619e1a39c330fb7
Nickenn <nickeavery@protonmail.com>
2022-08-28 23:25:46 +0200

    Nicholas Avery, Js1, Ma3

---
+++ js/question-1.js
@@ -0,0 +1,7 @@
+//Question 1
+
+// function getRemainer(a,b) {
+//     return a % b;
+// }
+
+const getRemainer = (a,b) => a % b;
---
+++ js/question-2.js
@@ -0,0 +1,46 @@
+//Question 2
+
+const CORS_URL = "https://noroffcors.herokuapp.com/";
+
+const API_KEY = "7efc6cd74c2d4a1b974b83e8ad1d3e64";
+const API_URL = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${API_KEY}`;
+
+
+const URL = CORS_URL + API_URL;
+
+const resultsContainer = document.querySelector(".container");
+const gameResults = document.querySelector(".result");
+
+async function getGames() {
+    try {
+        const response = await fetch(URL);
+        const data = await response.json();
+
+        console.log(data);
+
+        const games = data.all;
+
+        gameResults.innerHTML = "";
+
+        for (let i = 0;i < games; i++) {
+            console.log(games[i].text);
+
+                if (i === 8) {
+                        break;
+                    }
+        }
+
+   gameResults.innerHTML += `<div cless="result"> 
+    <p>Name: ${games} </p>
+    <p>Rating: ${games} </p>
+    <p>Number of tags: ${games}</p>
+    </div>`;
+
+    } catch (error) {
+        console.log("An error has occured", error);
+        resultsContainer.innerHTML += displayError("Bad things happened");
+    }
+}
+
+getGames();
+
